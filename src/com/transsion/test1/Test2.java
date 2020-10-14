98b416a98c4b89c7b09292db0c1c2441b7eeb76a
anjiangwei <jiangwei.an@transsion.com>
2020-10-14 14:27:34 +0800

    初始化项目

--- src/com/transsion/test1/Test2.java
+++ src/com/transsion/test1/Test2.java
@@ -3,5 +3,9 @@ package com.transsion.test1;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+
     }
 }
